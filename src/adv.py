d826930a1054f6785be80f9f5977b2f27c28b233
Vrndavana <Vrmdavama.th@gmail.com>
2020-06-03 14:17:52 -0700

    Adventure Directions and Quit

--- src/adv.py
+++ src/adv.py
@@ -1,5 +1,5 @@
 from room import Room
-
+from player import Player
 # Declare all the rooms
 
 room = {
@@ -38,9 +38,33 @@ room['treasure'].s_to = room['narrow']
 #
 
 # Make a new player object that is currently in the 'outside' room.
+player = Player('Cole', room['outside'])
+
 
 # Write a loop that:
-#
+while True:
+    direction = input("Where you going n, e, s, w?")
+    if direction == "n":
+        if player.current_room == None:
+            continue 
+        player.current_room = player.current_room.n_to
+    elif direction == "e":
+        if player.current_room == None:
+            continue
+        player.current_room = player.current_room.e_to
+    elif direction == "s":
+        if player.current_room == None:
+            continue
+        player.current_room = player.current_room.s_to
+    elif direction == "w":
+        if player.current_room == None:
+            continue
+    elif direction == "q":
+        print("You Quit")
+        break
+        player.current_room = player.current_room.w_to
+    print(player.current_room)
+    
 # * Prints the current room name
 # * Prints the current description (the textwrap module might be useful here).
 # * Waits for user input and decides what to do.
